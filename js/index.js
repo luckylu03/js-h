8cba0bb6abbb54b69c37661a02a64e47eec35be4
Vlad Starovierov <starovierov0311@gmail.com>
2018-11-21 14:37:57 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -4,22 +4,22 @@ const c = parseFloat(prompt('enter c:'));
 
 const d = b ** 2 - 4 * a * c;
 
-if (a!==0 && b!==0 && c!==0 && d>0) {
+if (a!===0 && b!===0 && c!===0 && d>0) {
     const x1 = calcSol(a, b, c, d);
     const x2 = calcSol2(a, b, c, d);
     alert('x1= ' + x1 + ' x2= ' + x2);
-} else if (a!==0 && b!==0 && c!==0 && d==0) {
+} else if (a!===0 && b!===0 && c!===0 && d===0) {
     const x = calcx (a, b, c, d);
     alert('x= ' + x + ' is the solution');
-} else if (a!==0 && b!==0 && c!==0 && d<0) {
+} else if (a!===0 && b!===0 && c!===0 && d<0) {
     alert('No solutions');
-} else if (a==0) {
+} else if (a===0) {
     const x = calcIfa (b, c);
     alert('x= ' + x + ' is the solution');
-} else if (b==0) {
+} else if (b===0) {
     const x = calcIfb(a, c);
     alert('x= ' + x + ' is the solution');
-} else if (c==0) {
+} else if (c===0) {
     const x2 = calcIfc(a, b);
     alert('x1= ' + 0 + ' x2= ' + x2);
 } else {
